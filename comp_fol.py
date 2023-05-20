a1a6f4ee47161b44bc30800f36d60ae5d6ad3526
Ana <acastan1@swarthmore.edu>
2023-05-19 20:43:21 -0500

    umm figuring it out and got it kinda working??

---
+++ comp_fol.py
@@ -0,0 +1,32 @@
+"""
+compare followers and following
+"""
+def main():
+	ers_path = 'followers.txt'
+	ing_path = 'following.txt'
+
+	with open(ers_path, 'r') as file:
+		ers = file.readlines()
+	file.close()
+
+	with open(ing_path, 'r') as file:
+		ing = file.readlines()
+	file.close()
+
+	for i in ers:
+		if i[0].isupper():
+			ers.remove(i)
+	
+	for i in ing:
+		if i[0].isupper():
+			ing.remove(i)
+
+
+	in_ers = list(set(ers) - set(ing))
+	in_ing = list(set(ing) - set(ers))
+
+	not_in_both = in_ers + in_ing
+
+	print("not mutual followers: ", not_in_both)
+
+main()
